f37f23248c4fa3aa5f3240612213b236abeb7649
Zia-Ul-Qamar-Tech-Notes <zia.softwareprogrammer@gmail.com>
2025-03-10 11:26:11 +0500

    dbcommit

--- src/index.js
+++ src/index.js
@@ -8,10 +8,7 @@ import { todoRouter } from "./routes/todo.js";
 const app = express();
 dotenv.config();
 const port = process.env.PORT || 5000;
-const connectDB = async () =>
-  await mongoose.connect(process.env.MONGODB_URI, {
-    dbName: "checking",
-  });
+const connectDB = async () => await mongoose.connect(process.env.MONGODB_URI);
 
 connectDB().then(console.log("Connected to DB"));
 
