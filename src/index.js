5f8e32b7248ad9bda5147e6b7bcf39218ac5e6c9
Zia-Ul-Qamar-Tech-Notes <zia.softwareprogrammer@gmail.com>
2025-03-10 13:15:01 +0500

    commiting

--- src/index.js
+++ src/index.js
@@ -18,13 +18,12 @@ const connectDB = async () =>
 connectDB().then(console.log("Connected to DB"));
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // If using cookies or authentication
-  })
-);
+app.use(cors());
+// {
+//   origin: "https://todofrontend.z30.web.core.windows.net/",
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true, // If using cookies or authentication
+// }
 
 app.use("/auth", UserRouter);
 app.use("/todo", todoRouter);
